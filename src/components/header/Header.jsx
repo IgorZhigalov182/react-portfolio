2cac24d8c195b6198bf3ec118ce1b8f36af610cc
Igor <zhigalovzenit112233@gmail.com>
2023-01-04 23:46:24 +0300

    add main page (withoit adaptive)

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -30,11 +30,11 @@ const Header = () => {
                                 <i className="uil uil-file-alt nav__icon"></i>Skills
                             </a>
                         </li>
-                        <li className="nav__item">
+                        {/* <li className="nav__item">
                             <a href="#services" className="nav__link">
                                 <i className="uil uil-briefcase-alt nav__icon"></i>Services
                             </a>
-                        </li>
+                        </li> */}
 
                         <li className="nav__item">
                             <a href="#portfolio" className="nav__link">
@@ -49,7 +49,9 @@ const Header = () => {
                         </li>
                     </ul>
 
-                    <i className="uil uil-times nav__close" onClick={()=> showMenu(!Toggle)}></i>
+                    <i className="uil uil-times nav__close" 
+                    onClick={()=> showMenu(!Toggle)}>
+                    </i>
                 </div>
                 <div className="nav__toggle" onClick={()=> showMenu(!Toggle)}>
                     <i className="uil uil-apps"></i>
