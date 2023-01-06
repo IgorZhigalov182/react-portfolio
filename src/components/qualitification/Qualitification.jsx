3a619d98cf562d1e1d07b3f6c4b39c8998249a73
Igor <zhigalovzenit112233@gmail.com>
2023-01-06 15:28:07 +0300

    add some style qualitifiaction

--- src/components/qualitification/Qualitification.jsx
+++ src/components/qualitification/Qualitification.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './qualification.css'
+import './qualitification.css'
 
 const Qulitification = () => {
   return (
@@ -21,10 +21,10 @@ const Qulitification = () => {
         </div>
 
         <div className="qualification__sections">
-          <div className="qualification__content">
+          <div className="qualification__content qualification__content-active">
             <div className="qualification__data">
               <div>
-                <h3 className="qualification__title"></h3>
+                <h3 className="qualification__title">Web design</h3>
                 <span className='qualification__subtitle'>LETI SPB</span>
                 <div className="qualification__calendar">
                   <i className='uil uil-calendar-alt'></i>
@@ -34,13 +34,123 @@ const Qulitification = () => {
 
               <div>
                 <span className="qualification__rounder"></span>
-                <div className="qualification__line"></div>
+                <span className="qualification__line"></span>
               </div>
             </div>
+
+            <div className="qualification__data">
+              <div></div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+
+              <div>
+                <h3 className="qualification__title">Enginer</h3>
+                <span className='qualification__subtitle'>LETI SPB</span>
+                <div className="qualification__calendar">
+                  <i className='uil uil-calendar-alt'></i>
+                  2021 - Leti
+                </div>
+              </div>
+
+              
+            </div>
+
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">Web dev</h3>
+                <span className='qualification__subtitle'>LETI SPB</span>
+                <div className="qualification__calendar">
+                  <i className='uil uil-calendar-alt'></i>
+                  2021 - Leti
+                </div>
+              </div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+
+            <div className="qualification__data">
+              <div></div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+
+              <div>
+                <h3 className="qualification__title">Enginer 2</h3>
+                <span className='qualification__subtitle'>LETI SPB</span>
+                <div className="qualification__calendar">
+                  <i className='uil uil-calendar-alt'></i>
+                  2021 - 2022 Kulak
+                </div>
+              </div>
+
+              
+            </div>
+          </div>
+
+          <div className="qualification__content">
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">Product designer</h3>
+                <span className='qualification__subtitle'>Microsoft</span>
+                <div className="qualification__calendar">
+                  <i className='uil uil-calendar-alt'></i>
+                  2021 - Leti
+                </div>
+              </div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+
+            <div className="qualification__data">
+              <div></div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+
+              <div>
+                <h3 className="qualification__title">Enginer 3 cat</h3>
+                <span className='qualification__subtitle'>Gidra</span>
+                <div className="qualification__calendar">
+                  <i className='uil uil-calendar-alt'></i>
+                  2022 - Leti
+                </div>
+              </div>
+
+              
+            </div>
+
+            <div className="qualification__data">
+              <div>
+                <h3 className="qualification__title">Web makaka</h3>
+                <span className='qualification__subtitle'>kul</span>
+                <div className="qualification__calendar">
+                  <i className='uil uil-calendar-alt'></i>
+                  2021 - Leti
+                </div>
+              </div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+
+
           </div>
         </div>
-
-        
       </div>
     </section>
 
