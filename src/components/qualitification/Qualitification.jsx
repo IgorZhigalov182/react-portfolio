a708a43ed0d1c331967a6e77e5db5b7eed608dc6
Igor <zhigalovzenit112233@gmail.com>
2023-01-07 10:28:11 +0300

    finish qualification section

--- src/components/qualitification/Qualitification.jsx
+++ src/components/qualitification/Qualitification.jsx
@@ -15,19 +15,34 @@ const Qulitification = () => {
 
       <div className="qualification__container container">
         <div className="qualification__tabs">
-          <div className={toggleState === 1}>
+          <div 
+          className={toggleState === 1 
+          ? 'qualification__button qualification__active button--flex' 
+          : 'qualification__button button--flex'}
+          onClick = {()=> toggleTab(1)}
+          >
             <i className="uil uil-graduation-cap qualification__icon"></i>
             Education
           </div>
 
-          <div className="qualification__button button--flex">
+          <div className= {
+          toggleState === 2
+          ? 'qualification__button qualification__active button--flex' 
+          : 'qualification__button button--flex'}
+          onClick = {()=> toggleTab(2)}
+          >
             <i className="uil uil-briefcase-alt qualification__icon"></i>
             Expirience
           </div>
         </div>
 
         <div className="qualification__sections">
-          <div className="qualification__content qualification__content-active">
+          <div 
+          className={toggleState === 1 
+          ? 'qualification__content qualification__content-active' 
+          : 'qualification__content'
+          }
+          >
             <div className="qualification__data">
               <div>
                 <h3 className="qualification__title">Web design</h3>
@@ -101,7 +116,10 @@ const Qulitification = () => {
             </div>
           </div>
 
-          <div className="qualification__content">
+          <div className={toggleState === 2
+          ? 'qualification__content qualification__content-active' 
+          : 'qualification__content'
+          }>
             <div className="qualification__data">
               <div>
                 <h3 className="qualification__title">Product designer</h3>
