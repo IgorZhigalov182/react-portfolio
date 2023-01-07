58d6301cddff60116750636790d6b5d0f806755d
Igor <zhigalovzenit112233@gmail.com>
2023-01-07 12:27:26 +0300

    add work section and qualification section change

--- src/components/work/WorkItems.jsx
+++ src/components/work/WorkItems.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 
-const WorkItems = () => {
+const WorkItems = ({item}) => {
   return (
-    <div>WorkItems</div>
+    <div className="work__card" key={item.id}>
+        <img src={item.image} 
+        alt="image" 
+        className='work__img' 
+        />
+        <h3 className="work__title">{item.title}</h3>
+        <a href="#" className="work__button">
+            Demo <i className="bx bx-right-arrow-alt work__button-icon"></i>
+        </a>
+    </div>
   )
 }
 
